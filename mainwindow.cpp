095c6501d1615277dd0bc3c3d299d572032049b7
Olivia <c2232430@gmail.com>
2020-06-01 00:12:47 -0700

    map ok can't roll compile ok

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,14 +1,52 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
-MainWindow::MainWindow(QWidget *parent) :
-    QMainWindow(parent),
-    ui(new Ui::MainWindow)
+#include <QDebug>
+#include <QGraphicsView>
+#include <QGraphicsScene>
+#include <QGraphicsItem>
+
+#define GAME_WIDTH  500
+#define GAME_HEIGHT 700
+#define GAME_TITLE "Doodle JuJuJump"
+
+
+//QGraphicsScene scene;
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+ //   , ui(new Ui::MainWindow)
+    , BackgroundPixmap_1(":/new/prefix1/image/temp1406589445.png")
+    , BackgroundPixmap_2(":/new/prefix1/image/temp1406589445.png")
+    , Player(":/new/prefix1/image/pngwing.com.png")
+
+
 {
-    ui->setupUi(this);
+   // ui->setupUi(this);
+  //  BackgroundPixmap=BackgroundPixmap.scaled(400,400);
+
+    InitialScene();
 }
 
 MainWindow::~MainWindow()
 {
-    delete ui;
+
+ //   delete ui;
+}
+
+void MainWindow::paintEvent(QPaintEvent *)
+{
+    QPainter painter(this);
+  //   QPainter painter_2(this);
+    painter.drawPixmap(this->rect(),BackgroundPixmap_1);
+    painter.drawPixmap(this->rect(),BackgroundPixmap_2);
+
+}
+
+void MainWindow::InitialScene()
+{
+//    MainWindow::PaintEvent(&BackgroundPixmap);
+    setFixedSize(GAME_WIDTH,GAME_HEIGHT);
+    setWindowTitle(GAME_TITLE);
+
 }
--- mainwindow.h
+++ mainwindow.h
@@ -13,10 +13,15 @@ class MainWindow : public QMainWindow
 
 public:
     explicit MainWindow(QWidget *parent = nullptr);
+    void paintEvent(QPaintEvent *);
+    void InitialScene();
     ~MainWindow();
 
+
 private:
-    Ui::MainWindow *ui;
+ //   Ui::MainWindow *ui;
+    QPixmap BackgroundPixmap_1;
+    QPixmap BackgroundPixmap_2;
 };
 
 #endif // MAINWINDOW_H
